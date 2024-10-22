ab39351d6c72104bde90d2440be29b7d160dbe45
mrlawsuit <opronin1992@gmail.com>
2024-10-22 22:34:12 +0200

    atach selery to tasks

--- app/tasks/celery_app.py
+++ app/tasks/celery_app.py
@@ -1,4 +1,10 @@
 from celery import Celery
 
 
-celery_app = Celery()
+celery_app = Celery('car_shering')
+
+# загрузка конфигурации селери из модуля
+celery_app.config_from_object('celery_config')
+
+# Автоматическая регистрация задач из модуля
+celery_app.autodiscover_tasks(['notifications'])
--- app/tasks/notifications.py
+++ app/tasks/notifications.py
@@ -1,33 +1,32 @@
-import requests
-from requests.auth import HTTPBasicAuth
-from celery import shared_task
-from app.models import User
-from email.mime.text import MIMEText
-
+import httpx
 import aiosmtplib
 
+from celery import shared_task
+from email.mime.text import MIMEText
 
 import app.database as database
-from app.config import SENDER_EMAIL, SMTP_HOST, SMTP_PORT, SMTP_PASS, SMS_SENDER_EMAIL, SMS_API, SMS_SIGN, SMS_URL
+import app.config as config
 
 
 @shared_task
 async def send_email_message(user_id, subject, message):
     async with database.get_session() as session:
-        user = await database.get_user_db_by_id(session, user_id)
+        user = await database.get_user_by_id_db(session, user_id)
         email = user.email
         msg = MIMEText(message)
         msg['Subject'] = subject
-        msg['From'] = SENDER_EMAIL
+        msg['From'] = config.SENDER_EMAIL
         msg['To'] = email
 
-        email_client = aiosmtplib.SMTP(hostname=SMTP_HOST, port=SMTP_PORT, use_tls=True)
+        email_client = aiosmtplib.SMTP(
+            hostname=config.SMTP_HOST, port=config.SMTP_PORT, use_tls=True
+        )
         await email_client.connect()
-        await email_client.login(SENDER_EMAIL, SMTP_PASS)
+        await email_client.login(config.SENDER_EMAIL, config.SMTP_PASS)
         await email_client.send_message(msg)
         await email_client.quit()
-    
-    return f'Email sent successfully to receiver.'
+
+    return 'Email sent successfully to receiver.'
 
 
 @shared_task
@@ -35,22 +34,25 @@ async def send_sms(user_id, message):
     async with database.get_session() as session:
         user = await database.get_user_by_id_db(session, user_id)
         phone = user.phone
+        if not phone:
+            raise ValueError("Телефон пользователя не найден.")
         headers = {
             'accept': 'application/json'
         }
         params = {
             'number': phone,
             'text': message,
-            'sign': SMS_SIGN
+            'sign': config.SMS_SIGN
         }
-        response = requests.get(
-            SMS_URL,
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            config.SMS_URL,
             headers=headers,
             params=params,
-            auth=HTTPBasicAuth(SMS_SENDER_EMAIL, SMS_API)
+            auth=(config.SMS_SENDER_EMAIL, config.SMS_API)
         )
-        if response.status_code == 200:
-            data = response.json()
-            print(data)
-        else:
-            print("Ошибка:", response.status_code, response.text)
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+    else:
+        print("Ошибка:", response.status_code, response.text)
