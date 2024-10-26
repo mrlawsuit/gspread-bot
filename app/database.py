565e13426582fe97bd33af54bc7bea66d63ec57e
mrlawsuit <opronin1992@gmail.com>
2024-10-26 17:00:27 +0200

    add get all available vehicles function

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import (
     create_async_engine, async_sessionmaker, AsyncSession
 )
 from sqlalchemy import select, update, delete
-from .models.users_model import User
+from .models import User, Vehicle
 from app.config import DB_URL
 
 engine = create_async_engine(DB_URL, echo=True)
@@ -16,6 +16,18 @@ async_session = async_sessionmaker(engine, expire_on_commit=False)
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
         yield session
+
+
+async def get_all_available_vehicles(
+        session: AsyncSession
+):
+    result = await session.execute(
+        select(Vehicle).where(Vehicle.status == 'available')
+    )
+    vehicles = result.all()
+    if not vehicles:
+        raise ValueError('Нет свободных автомобилей')
+    return vehicles
 
 
 async def get_user_by_id_db(
