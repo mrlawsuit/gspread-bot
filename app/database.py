0f43b535bc888d4014a295312cbb2b1f56f3345b
mrlawsuit <opronin1992@gmail.com>
2024-10-27 22:28:30 +0200

    add get_available_vehicles, maintenance_status_check get_last_maintenance_by_id for database

--- app/database.py
+++ app/database.py
@@ -3,7 +3,8 @@ from typing import List, AsyncGenerator
 from sqlalchemy.ext.asyncio import (
     create_async_engine, async_sessionmaker, AsyncSession
 )
-from sqlalchemy import select, update, delete, desc
+from contextlib import asynccontextmanager
+from sqlalchemy import select, update, delete, desc, or_
 from .models import User, Vehicle, VehicleMaintenance
 from app.config import DB_URL
 
@@ -12,34 +13,60 @@ engine = create_async_engine(DB_URL, echo=True)
 
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
-
+@asynccontextmanager
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
         yield session
 
+# хэндлеры для таблицы машин и обслуживания
+async def get_available_vehicles():
+    async with get_session() as session:
+        result = await session.execute(
+            select(Vehicle).
+            where(Vehicle.status == 'available')
+        )
+        vehicles = result.scalars().all()
+        if not vehicles:
+            raise ValueError('Нет свободных автомобилей')
+        return vehicles
 
-async def get_maintenance_by_id(session: AsyncSession, vehicle_id: int):
-    result = await session.execute(
-        select(VehicleMaintenance).
-        where(VehicleMaintenance.vehicle_id == vehicle_id).
-        order_by(desc(VehicleMaintenance.id))
-    )
-    maintenance = result.scalars().first()
-    if not maintenance:
-        raise ValueError('У машины ещё не проводилось обслуживания')
-    return maintenance
 
-async def get_all_available_vehicles(
-        session: AsyncSession
-):
-    result = await session.execute(
-        select(Vehicle).
-        where(Vehicle.status == 'available')
-    )
-    vehicles = result.all()
-    if not vehicles:
-        raise ValueError('Нет свободных автомобилей')
-    return vehicles
+# вернет false, если есть акттивные заявки на обслуживание
+async def maintenance_status_check(vehicle_id: int):
+    async with async_session() as session:
+        result = await session.execute(
+            select(VehicleMaintenance).
+            where(
+                VehicleMaintenance.vehicle_id == vehicle_id,
+                or_(
+                    VehicleMaintenance.status == 'in_process',
+                    VehicleMaintenance.status == 'planned'
+                )
+            )
+        )
+        maintenances = result.scalars().all()
+        if maintenances:
+            return False
+        return True
+
+
+# вернет кортеж из даты обслуживания и пробега на момент обслуживания
+async def get_last_maintenance_by_id(vehicle_id: int):
+    async with async_session() as session:
+        result = await session.execute(
+            select(VehicleMaintenance).
+            where(
+                VehicleMaintenance.vehicle_id == vehicle_id,
+            ).
+            order_by(desc(VehicleMaintenance.service_date))
+        )
+        maintenance = result.scalars().first()
+
+        return (maintenance.service_date, maintenance.current_mileage)
+
+
+
+
 
 
 async def get_user_by_id_db(
