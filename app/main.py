2faf9ed7800dd5094b947ff22357aeaa15362308
mrlawsuit <opronin1992@gmail.com>
2024-11-04 20:52:59 +0200

    fixing typos

--- app/main.py
+++ app/main.py
@@ -12,6 +12,9 @@ async def root():
 
 
 @app.get('/users/{id}')
-async def get_user_by_id(id: int, session: AsyncSession = Depends(database.get_session)):
+async def get_user_by_id(
+    id: int,
+    session: AsyncSession = Depends(database.get_session)
+):
     user = await database.get_user_by_id_db(session, id)
     return user.email
