bcbd0bd6aed27cceb054ec64d23dce6908702ac6
mrlawsuit <opronin1992@gmail.com>
2024-11-10 12:00:30 +0200

    add elastic_init function

--- app/search/elasticksearch.py
+++ app/search/elasticksearch.py
@@ -1,1 +1,9 @@
-import elasticsearch
+from elasticsearch import AsyncElasticsearch
+
+
+elastic_client = AsyncElasticsearch
+
+
+async def elastic_init(index, mapping):
+    if not elastic_client.exists(index):
+        elastic_client.create(index=index, body=mapping)
