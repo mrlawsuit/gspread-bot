a48f296b955969a04fc517ff5030015477b9b52b
mrlawsuit <opronin1992@gmail.com>
2024-11-11 17:17:04 +0200

    add enum_to_string method to Base model

--- app/models/base_model.py
+++ app/models/base_model.py
@@ -1,7 +1,15 @@
+from enum import Enum
+
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncAttrs
 
 
 class Base(AsyncAttrs, DeclarativeBase):
     def to_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        return {c.name: self.enum_to_string(getattr(self, c.name)) for c in self.__table__.columns}    
+
+    @staticmethod
+    def enum_to_string(value):
+        if isinstance(value, Enum):
+            return value.value
+        return value
